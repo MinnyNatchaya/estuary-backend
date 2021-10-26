d6e76bdf3af19f1067ea0bef69e687af271d4df1
lovely17145 <my_friend_pv@hotmail.com>
2021-10-26 16:10:53 +0700

    product

--- app.js
+++ app.js
@@ -1,38 +1,40 @@
-// const { sequelize } = require('./models');
+// const { sequelize } = require("./models");
 // sequelize.sync({ force: true });
 
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
-const errorController = require('./controller/errorController');
+const errorController = require("./controller/errorController");
 
-const productRoute = require('./routes/productRoute');
-const postRoute = require('./routes/postRoute');
-const profileRoute = require('./routes/profileRoute');
-const authRoute = require('./routes/authRoute');
+const productCategoryRoute = require("./routes/productCategoryRoute");
+const productRoute = require("./routes/productRoute");
+const postRoute = require("./routes/postRoute");
+const profileRoute = require("./routes/profileRoute");
+const authRoute = require("./routes/authRoute");
 
-const passport = require('passport');
-require('./config/passport');
+const passport = require("passport");
+require("./config/passport");
 app.use(passport.initialize());
 
 app.use(cors());
 app.use(express.json());
 
 //////////////////omise/////////////////////////////////////
-const omiseRoute = require('./routes/omiseRoute');
-app.use('/checkout-credit-card', omiseRoute);
+const omiseRoute = require("./routes/omiseRoute");
+app.use("/checkout-credit-card", omiseRoute);
 
 ///////////////////////////////////////////////////////
 
-app.use('/product', productRoute);
-app.use('/post', postRoute);
-app.use('/profile', profileRoute);
-app.use('/', authRoute);
+app.use("/category", productCategoryRoute);
+app.use("/product", productRoute);
+app.use("/post", postRoute);
+app.use("/profile", profileRoute);
+app.use("/", authRoute);
 
 app.use((req, res, next) => {
-  res.status(404).json({ message: 'resource not found on this server' });
+  res.status(404).json({ message: "resource not found on this server" });
 });
 
 app.use(errorController);
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,23 +1,35 @@
-const express = require('express');
-const productController = require('../controller/productController');
-const { upload } = require('../middleware/uploadFile');
+const express = require("express");
+const productController = require("../controller/productController");
+const { upload } = require("../middleware/uploadFile");
 const router = express.Router();
-const passport = require('passport');
+const passport = require("passport");
 
-router.get('/', passport.authenticate('jwt', { session: false }), productController.getAllProducts);
-router.get('/:id', passport.authenticate('jwt', { session: false }), productController.getProductById);
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  productController.getAllProducts
+);
+router.get(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  productController.getProductById
+);
 router.post(
-  '/',
-  passport.authenticate('jwt', { session: false }),
-  upload.single('coverPic'),
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  upload.single("coverPic"),
   productController.createProduct
 );
 router.put(
-  '/:id',
-  passport.authenticate('jwt', { session: false }),
-  upload.single('coverPic'),
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  upload.single("coverPic"),
   productController.updateProduct
 );
-router.delete('/:id', passport.authenticate('jwt', { session: false }), productController.deleteProduct);
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  productController.deleteProduct
+);
 
 module.exports = router;
