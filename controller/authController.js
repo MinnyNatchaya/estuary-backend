1093d8ca222ac10c7ccdaf30c118c9bed0c0566f
nattapat.aur <nattapat.aur@gmail.com>
2021-11-03 15:40:20 +0700

    fixed-desktopMenu

--- controller/authController.js
+++ controller/authController.js
@@ -1,6 +1,6 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { User } = require('../models');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { User, Member } = require("../models");
 
 // exports.authenticate = async (req, res, next) => {
 // 	try {
@@ -33,96 +33,112 @@ const { User } = require('../models');
 // };
 
 exports.signup = async (req, res, next) => {
-  try {
-    const { username, password, email } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
+	try {
+		const { username, password, email } = req.body;
+		const hashedPassword = await bcrypt.hash(password, 10);
 
-    console.log(req.body);
+		console.log(req.body);
 
-    await User.create({
-      username,
-      password: hashedPassword,
-      email,
-      role: 'CLIENT'
-    });
-    res.status(200).json({ message: 'Your account has been created' });
-  } catch (err) {
-    next(err);
-  }
+		await User.create({
+			username,
+			password: hashedPassword,
+			email,
+			role: "CLIENT",
+		});
+		res.status(200).json({ message: "Your account has been created" });
+	} catch (err) {
+		next(err);
+	}
 };
 
 exports.login = async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
-    const user = await User.findOne({ where: { username: username } });
-    if (!user) {
-      return res.status(400).json({ message: 'Incorrect username or password', name: 'loginError' });
-    }
+	try {
+		const { username, password } = req.body;
+		const user = await User.findOne({ where: { username: username } });
+		const ownedCommunityId = await Member.findOne({
+			where: { userId: user.id, role: "OWNER" },
+			attributes: ["communityId"],
+		});
+		if (!user) {
+			return res.status(400).json({ message: "Incorrect username or password", name: "loginError" });
+		}
 
-    const isPasswordCorrect = await bcrypt.compare(password, user.password);
-    if (!isPasswordCorrect) {
-      return res.status(400).json({ message: 'Incorrect username or password', name: 'loginError' });
-    }
-    console.log(JSON.stringify(user, null, 2));
+		const isPasswordCorrect = await bcrypt.compare(password, user.password);
+		if (!isPasswordCorrect) {
+			return res.status(400).json({ message: "Incorrect username or password", name: "loginError" });
+		}
+		console.log(JSON.stringify(user, null, 2));
 
-    const payload = {
-      id: user.id,
-      username: user.username,
-      role: user.role,
-      profilePic: user.profilePic
-    };
-    const secretKey = process.env.JWT_SECRET_KEY;
-    const token = jwt.sign(payload, secretKey, { expiresIn: 60 * 60 * 24 });
+		const payload = {
+			id: user.id,
+			username: user.username,
+			role: user.role,
+			profilePic: user.profilePic,
+			ownedCommunityId: ownedCommunityId.communityId,
+		};
+		const secretKey = process.env.JWT_SECRET_KEY;
+		const token = jwt.sign(payload, secretKey, { expiresIn: 60 * 60 * 24 });
 
-    res.json({ message: 'login success', token });
-  } catch (err) {
-    next(err);
-  }
+		res.json({ message: "login success", token });
+	} catch (err) {
+		next(err);
+	}
 };
 
 exports.loginGoogle = async (req, res, next) => {
-  try {
-    const { username, password, email, firstName, lastName, profilePic } = req.body;
-    const user = await User.findOne({ where: { email: email } });
-    // console.dir(user);
-    if (!user) {
-      const hashedPassword = await bcrypt.hash(password, 10);
-      await User.create({
-        username,
-        password: hashedPassword,
-        email,
-        firstName,
-        lastName,
-        profilePic,
-        isGoogleAccount: true,
-        role: 'CLIENT'
-      });
-      // res.status(200).json({ message: 'Your account has been created' });
-      const user2 = await User.findOne({ where: { email: email } });
+	try {
+		const { username, password, email, firstName, lastName, profilePic } = req.body;
+		const user = await User.findOne({ where: { email: email } });
+		// console.dir(user);
+		if (!user) {
+			const hashedPassword = await bcrypt.hash(password, 10);
+			await User.create({
+				username,
+				password: hashedPassword,
+				email,
+				firstName,
+				lastName,
+				profilePic,
+				isGoogleAccount: true,
+				role: "CLIENT",
+			});
+			// res.status(200).json({ message: 'Your account has been created' });
+			const user2 = await User.findOne({ where: { email: email } });
 
-      const payload = {
-        id: user2.id,
-        username: user2.username,
-        role: user2.role,
-        profilePic: user2.profilePic
-      };
-      const secretKey = process.env.JWT_SECRET_KEY;
-      const token = jwt.sign(payload, secretKey, { expiresIn: 60 * 60 * 24 });
+			const ownedCommunityId = await Member.findOne({
+				where: { userId: user2.id, role: "OWNER" },
+				attributes: ["communityId"],
+			});
 
-      res.json({ message: 'login success', token });
-    }
+			const payload = {
+				id: user2.id,
+				username: user2.username,
+				role: user2.role,
+				profilePic: user2.profilePic,
+				ownedCommunityId: ownedCommunityId.communityId,
+			};
+			const secretKey = process.env.JWT_SECRET_KEY;
+			const token = jwt.sign(payload, secretKey, { expiresIn: 60 * 60 * 24 });
 
-    const payload = {
-      id: user.id,
-      username: user.username,
-      role: user.role,
-      profilePic: user.profilePic
-    };
-    const secretKey = process.env.JWT_SECRET_KEY;
-    const token = jwt.sign(payload, secretKey, { expiresIn: 60 * 60 * 24 });
+			return res.json({ message: "login success", token });
+		}
+		const ownedCommunityId = await Member.findOne({
+			where: { userId: user.id, role: "OWNER" },
+			attributes: ["communityId"],
+		});
 
-    res.json({ message: 'login success', token });
-  } catch (err) {
-    next(err);
-  }
+		const payload = {
+			id: user.id,
+			username: user.username,
+			role: user.role,
+			profilePic: user.profilePic,
+			ownedCommunityId: ownedCommunityId.communityId,
+		};
+		const secretKey = process.env.JWT_SECRET_KEY;
+		const token = jwt.sign(payload, secretKey, { expiresIn: 60 * 60 * 24 });
+
+		return res.json({ message: "login success", token });
+	} catch (err) {
+		next(err);
+	}
 };
